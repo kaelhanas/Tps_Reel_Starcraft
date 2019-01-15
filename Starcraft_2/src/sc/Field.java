581ac873dd1ac20b086ab41ca0267d9aef39d8c2
kaelhanas <gkylian@gmail.com>
2019-01-15 18:41:29 +0100

    rework du projet en Multi IA, Les deplacements aléatoire sont up, la detection des enemy en range est up, manque la detection dans lautre sens et tuer une unité

--- Starcraft_2/src/sc/Field.java
+++ Starcraft_2/src/sc/Field.java
@@ -3,6 +3,7 @@ package sc;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,25 +13,25 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-
 public class Field extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 112737L;
-	private ArrayList<Svc> svcs =  new ArrayList<Svc>();
-	private ArrayList<Mineral> minerals =  new ArrayList<Mineral>();
-	private ArrayList<Center> centers =  new ArrayList<Center>();
+	public ArrayList<Svc> svcs_J1 = new ArrayList<Svc>();
+	public ArrayList<Svc> svcs_J2 = new ArrayList<Svc>();
+
+	private ArrayList<Center> centers = new ArrayList<Center>();
 	private Random rand = new Random(123456);
 	public int XMAX = 10;
 	public int YMAX = 10;
 	public int step = 64;
-	
+
 	public static BufferedImage ground;
-	
+
 	public static int X_SVC_position = 6;
 	public static int Y_SVC_position = 3;
-	
+
 	static {
 		try {
 			ground = ImageIO.read(new File("res/ground.jpeg"));
@@ -40,115 +41,116 @@ public class Field extends JPanel {
 	}
 
 	public Field() {
-		generateSvc(4, Joueurs.J1);
-		generateSvc(4, Joueurs.J2);
+		generateSvc(10, Joueurs.J1);
+		generateSvc(10, Joueurs.J2);
 		generateCenters(1);
 	}
-	
+
 	public void generateCenters(int n) {
-		Center center1 = new Center(0,0,this,0, Joueurs.J1);
+		Center center1 = new Center(0, 0, this, 0, Joueurs.J1);
 		centers.add(center1);
-		Center center2 = new Center(7,7,this,1, Joueurs.J2);
+		Center center2 = new Center(7, 7, this, 1, Joueurs.J2);
 		centers.add(center2);
 	}
-	
+
 	public void generateSvc(int n, Joueurs joueur) {
-		if (joueur.equals(Joueurs.J1)) 
-		{
+		if (joueur.equals(Joueurs.J1)) {
 			for (int i = 0; i < n; i++) {
 				Svc svc = new Svc(0, 0, this, i, joueur);
-				//Svc svc = new Svc(rand.nextInt(XMAX),rand.nextInt(YMAX),this,i);
-				svcs.add(svc);
+				// Svc svc = new Svc(rand.nextInt(XMAX),rand.nextInt(YMAX),this,i);
+				svcs_J1.add(svc);
 				new Thread(svc).start();
-			} 
+			}
 		}
-		
+
 		else {
 			for (int i = 0; i < n; i++) {
 				Svc svc = new Svc(9, 9, this, i, joueur);
-				//Svc svc = new Svc(rand.nextInt(XMAX),rand.nextInt(YMAX),this,i);
-				svcs.add(svc);
+				// Svc svc = new Svc(rand.nextInt(XMAX),rand.nextInt(YMAX),this,i);
+				svcs_J2.add(svc);
 				new Thread(svc).start();
-			} 
-		}
-	}
-	
-	public void generateMinerals(int n) {
-		for (int i = 0; i < n; i++) {
-			Mineral flower = new Mineral(rand.nextInt(XMAX),rand.nextInt(YMAX),this,i);
-			minerals.add(flower);
-		}
-	}
-	
-	public void paintComponent(Graphics g) {
-	  super.paintComponent(g);
-	  
-	  Graphics2D g2 = (Graphics2D) g;
-	  
-	  for (int i = 0; i < XMAX; i++) {
-		for (int j = 0; j < YMAX; j++) {
-			g2.drawImage(ground, i*step, j*step, null);
-		}
-	  }
-	  
-	  g2.setColor(Color.GREEN);
-	  
-	  // draw centers
-	  for (Center center : centers) {
-		 try {
-			g2.drawImage(Center.getImg(), center.getX()*step, center.getY()*step, null);
-		} catch (IllegalPlayerTypeArgument e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 g2.drawString(center.getNbDeposit()+"", center.getX()*step+15, center.getY()*step+15);
-	  }
-	  
-	  // draw SVC
-	  for (Svc svc : svcs) {
-		 g2.drawImage(svc.getImg(), svc.getX()*step, svc.getY()*step, null);
-		 g2.drawString(svc.getId()+"", svc.getX()*step+25, svc.getY()*step+10);
-	  }
-	}
-	
-	public void checkSvcOnMineral(Svc svc) {
-		if(svc.isFull())
-			return;
-		
-		for (Mineral flower : minerals) {
-			if((flower.getX() == svc.getX()) && (flower.getY() == svc.getY())) {
-				svc.gather(flower);
-				break;
 			}
 		}
 	}
-	
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2 = (Graphics2D) g;
+
+		for (int i = 0; i < XMAX; i++) {
+			for (int j = 0; j < YMAX; j++) {
+				g2.drawImage(ground, i * step, j * step, null);
+			}
+		}
+
+		g2.setColor(Color.GREEN);
+
+		// draw centers
+		for (Center center : centers) {
+			try {
+				g2.drawImage(Center.getImg(), center.getX() * step, center.getY() * step, null);
+			} catch (IllegalPlayerTypeArgument e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			g2.drawString(center.getNbDeposit() + "", center.getX() * step + 15, center.getY() * step + 15);
+		}
+
+		// draw SVC
+		for (Svc svc : svcs_J1) {
+			g2.drawImage(svc.getImg(), svc.getX() * step, svc.getY() * step, null);
+			g2.drawString(svc.getId() + "", svc.getX() * step + 25, svc.getY() * step + 10);
+		}
+
+		for (Svc svc : svcs_J2) {
+			g2.drawImage(svc.getImg(), svc.getX() * step, svc.getY() * step, null);
+			g2.drawString(svc.getId() + "", svc.getX() * step + 25, svc.getY() * step + 10);
+		}
+	}
+
 	public Center getOneCenter() {
 		return this.centers.get(rand.nextInt(centers.size()));
 	}
-	
-	public Mineral getOneMineral() {
-		return this.minerals.get(rand.nextInt(minerals.size()));
-	}
-	
+
 	public void checkSvcOnCenter(Svc svc) {
-		if(!svc.isFull()) {
+		if (!svc.isFull()) {
 			return;
 		}
-		
-		for(Center center: centers) {
-			if((center.getX() == svc.getX()) && (center.getY() == svc.getY())) {
+
+		for (Center center : centers) {
+			if ((center.getX() == svc.getX()) && (center.getY() == svc.getY())) {
 				svc.deposit(center);
 			}
-			if((center.getX() == svc.getX()+1) && (center.getY() == svc.getY())) {
+			if ((center.getX() == svc.getX() + 1) && (center.getY() == svc.getY())) {
 				svc.deposit(center);
 			}
-			if((center.getX() == svc.getX()) && (center.getY()+1 == svc.getY())) {
+			if ((center.getX() == svc.getX()) && (center.getY() + 1 == svc.getY())) {
 				svc.deposit(center);
 			}
-			if((center.getX()+1 == svc.getX()) && (center.getY()+1 == svc.getY())) {
+			if ((center.getX() + 1 == svc.getX()) && (center.getY() + 1 == svc.getY())) {
 				svc.deposit(center);
 			}
 		}
 	}
+
+	public void checkOnRangeEnemy(Svc svc) throws InterruptedException {
+
+		ArrayList<Point> svcRange = svc.getRange();
+
+		if (svc.getJoueur().equals(Joueurs.J1)) {
+			for (Svc enemy : svcs_J2) {
+				if (svcRange.contains(enemy.getPosition())) {
+					System.out.println("SVC" + svc.getId() + "J1 is on range of SVC" + enemy.getId() + "J2 FIRE!!!!");
+					//Thread.currentThread().sleep(1000000000);
+				}
+			}
+		}
+
+		else {
+
+		}
+
+	}
+
 }
--- Starcraft_2/src/sc/Svc.java
+++ Starcraft_2/src/sc/Svc.java
@@ -1,12 +1,13 @@
 package sc;
 
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
-
 
 public class Svc implements Runnable {
 	public BufferedImage img;
@@ -32,6 +33,12 @@ public class Svc implements Runnable {
 		}
 	}
 
+	public Svc(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	
 	public Svc(int x, int y, Field field, int id, Joueurs j) {
 		this.x = x;
 		this.y = y;
@@ -39,8 +46,10 @@ public class Svc implements Runnable {
 		this.id = id;
 		if (j.equals(Joueurs.J1)) {
 			this.img = imgSvcEmpty1;
+			this.j=j;
 		} else if (j.equals(Joueurs.J2)) {
 			this.img = imgSvcEmpty2;
+			this.j=j;
 		}
 		this.isFull = false;
 	}
@@ -51,7 +60,8 @@ public class Svc implements Runnable {
 			while (true) {
 				// svc behavior
 				Thread.sleep(500);
-				//tryToMoveWithTarget();
+				// tryToMoveWithTarget();
+				tryToMoveRandom();
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -63,50 +73,28 @@ public class Svc implements Runnable {
 		return this.isFull;
 	}
 
-	private void tryToMoveWithTarget() {
-		if (!isFull) {
-			if (this.target == null) {
-				target = this.field.getOneMineral();
-			}
-
-			int choice = rand.nextInt(2);
-			if (choice == 0 && x != target.getX()) {
-				if (x < target.getX() && x + 1 < field.XMAX)
-					x++;
-				else if (x - 1 >= 0)
-					x--;
-
-			} else if (choice == 1) {
-				if (y < target.getY() && y + 1 < field.YMAX)
-					y++;
-				else if (y - 1 >= 0)
-					y--;
-			}
-		} else {
-			int choice = rand.nextInt(2);
-			if (choice == 0 && x != field.getOneCenter().getX()) {
-				if (x < field.getOneCenter().getX() && x + 1 < field.XMAX)
-					x++;
-				else if (x - 1 >= 0)
-					x--;
-
-			} else if (choice == 1) {
-				if (y < field.getOneCenter().getY() && y + 1 < field.YMAX)
-					y++;
-				else if (y - 1 >= 0)
-					y--;
-			}
-		}
-
-		// refresh the field
-		field.repaint();
-
-		// check mineral
-		field.checkSvcOnMineral(this);
-
-		// check center
-		field.checkSvcOnCenter(this);
-	}
+	/*
+	 * private void tryToMoveWithTarget() { if (!isFull) { if (this.target == null)
+	 * { target = this.field.getOneMineral(); }
+	 * 
+	 * int choice = rand.nextInt(2); if (choice == 0 && x != target.getX()) { if (x
+	 * < target.getX() && x + 1 < field.XMAX) x++; else if (x - 1 >= 0) x--;
+	 * 
+	 * } else if (choice == 1) { if (y < target.getY() && y + 1 < field.YMAX) y++;
+	 * else if (y - 1 >= 0) y--; } } else { int choice = rand.nextInt(2); if (choice
+	 * == 0 && x != field.getOneCenter().getX()) { if (x <
+	 * field.getOneCenter().getX() && x + 1 < field.XMAX) x++; else if (x - 1 >= 0)
+	 * x--;
+	 * 
+	 * } else if (choice == 1) { if (y < field.getOneCenter().getY() && y + 1 <
+	 * field.YMAX) y++; else if (y - 1 >= 0) y--; } }
+	 * 
+	 * // refresh the field field.repaint();
+	 * 
+	 * // check mineral field.checkSvcOnMineral(this);
+	 * 
+	 * // check center field.checkSvcOnCenter(this); }
+	 */
 
 	private void tryToMoveRandom() {
 		int choice = rand.nextInt(4);
@@ -127,11 +115,18 @@ public class Svc implements Runnable {
 				y--;
 		}
 
-		// check mineral
-		field.checkSvcOnMineral(this);
+		field.repaint();
 
 		// check center
 		field.checkSvcOnCenter(this);
+
+		// check enemie on range
+		try {
+			field.checkOnRangeEnemy(this);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public BufferedImage getImg() {
@@ -160,6 +155,15 @@ public class Svc implements Runnable {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Joueurs getJoueur() {
+		return this.j;
+	}
+	
+	public Point getPosition()
+	{
+		return new Point(getX(),getY());
 	}
 
 	public void gather(Mineral mineral) {
@@ -201,4 +205,31 @@ public class Svc implements Runnable {
 		System.out.println("I'm svc " + id + ", I stop to deposit on center " + center.getId());
 		center.stopDeposit();
 	}
+	
+	
+	public ArrayList<Point> getRange()
+	{
+		ArrayList<Point> range = new ArrayList<Point>();
+		
+		range.add(new Point(getX()-1,getY()-1));
+		range.add(new Point(getX(),getY()-1));
+		range.add(new Point(getX()+1,getY()+1));
+		range.add(new Point(getX()-1,getY()));
+		range.add(new Point(getX()+1,getY()));
+		range.add(new Point(getX()-1,getY()+1));
+		range.add(new Point(getX(),getY()+1));
+		range.add(new Point(getX()+1,getY()+1));
+		
+		return range;
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Svc svc = new Svc(0, 0);
+		System.out.println(svc.getRange().toString());
+	}
 }
+
+
+
